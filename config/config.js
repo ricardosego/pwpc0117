7666fdceaf0a44477fad59cf030761a7a092611c
ricardo <ricardosegovia910@gmail.com>
2017-03-09 08:21:59 -0600

    00 iniciando proyecto de server

---
+++ config/config.js
@@ -0,0 +1,11 @@
+exports.module = {
+    "IP" : '127.0.0.1' || process.env.IP,
+    "PORT": 3000 || process.env.PORT,
+    "colo_theme": {
+        "info": "rainbow",
+        "data": "green",
+        "eror": "red",
+        "warning": "yellow"
+    },
+    "STATIC_PATH" : "./static"
+};
---
+++ static/js/site.js
@@ -0,0 +1,7 @@
+//este script sera ejecutado 
+//por el cliente (navegador)
+ 
+
+function saludar(){
+    alert("bienvenido a mi sitio ");
+}
